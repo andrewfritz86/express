88e28f07a93a61c8387525ff441a5ef467271d51
Andy Fritz <andrew.fritz86@gmail.com>
2022-10-13 15:43:22 -0400

    Add app mods

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ app.get("/", (req, res) => {
   res.send("hi")
 })
 
+app.get('/test', (req, res) => {
+  res.send("hello world!!")
+});
+
+app.get("/better-api", (req,res) => {
+  res.json({name: "pasha"})
+})
+
+app.post("/test", (req,res) =>{
+  res.send("HELLSLDLSL")
+})
+
+app.get("/dog-name", (req, res) => {
+  res.send(`<span>my dogs name is </span> <h1 style="color:red">Pasha </h1> <span> and he is cute </span>`)
+})
+
+app.get("/api", (req,res ) => {
+  res.send("DOG NAME IS PASHA")
+})
+
 // Listening?? WTF??
 app.listen(port, () => {
   console.log(`We are listening on ${port}`)
